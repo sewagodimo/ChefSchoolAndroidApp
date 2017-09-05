aaa9f9a886d278ee8547e9b935dd581730978970
Sewagodimo Matlapeng <edneciamat@gmail.com>
2017-09-05 11:11:44 +0200

    Edited the layout of the home page, added a profile image and add the sqlite class

--- java/com/example/mosadi/chefschool/userinformation/Profile.java
+++ java/com/example/mosadi/chefschool/userinformation/Profile.java
@@ -17,27 +17,48 @@ public class Profile {
     private String province;
     private String city;
     private String surburb;
-    private boolean activated;
+
     private String image;
     //Create a user
     public Profile(String id, String name,String surname, String contact, int classnr, String dob){//when a user register
-        this.userID=id;
+        this.setUserID(id);
         this.image="";//There is no image so far
-        this.name=name;
-        this.surname=surname;
-        this.class_number=classnr;
-        this.email="";
-        this.phone="";
-        this.contact=contact;
-        this.dob=dob;
-        this.country="South Africa";
-        this.province="Western Cape";
-        this.city="Cape Town";
-        this.surburb="";
+        this.setName(name);
+        this.setSurname(surname);
+        this.setClass_number(classnr);
+        this.setEmail("");
+        this.setPhone("");
+        this.setContact(contact);
+        this.setDob(dob);
+        this.setCountry("South Africa");
+        this.setProvince("Western Cape");
+        this.setCity("Cape Town");
+        this.setSurburb("");
         if(contact.contains("@")){
-            this.email=contact;
+            this.setEmail(contact);
         }
-        else{this.phone=contact;}
+        else{
+            this.setPhone(contact);}
+    }//end of constructor
+    public void edit_profile(String id, String name,String surname, String contact, int classnr, String dob){//when a user register
+        this.setUserID(id);
+        this.image="";//There is no image so far
+        this.setName(name);
+        this.setSurname(surname);
+        this.setClass_number(classnr);
+        this.setEmail("");
+        this.setPhone("");
+        this.setContact(contact);
+        this.setDob(dob);
+        this.setCountry("South Africa");
+        this.setProvince("Western Cape");
+        this.setCity("Cape Town");
+        this.setSurburb("");
+        if(contact.contains("@")){
+            this.setEmail(contact);
+        }
+        else{
+            this.setPhone(contact);}
     }//end of constructor
     public void edit_profile(){
         //get values from different parts and edit them
@@ -50,5 +71,102 @@ public class Profile {
     }
     public void cancelAddressChanges(){
 
+    }
+
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getClass_number() {
+        return class_number;
+    }
+
+    public void setClass_number(int class_number) {
+        this.class_number = class_number;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getSurburb() {
+        return surburb;
+    }
+
+    public void setSurburb(String surburb) {
+        this.surburb = surburb;
     }
 }
--- java/com/example/mosadi/chefschool/userinformation/StudentAccountContract.java
+++ java/com/example/mosadi/chefschool/userinformation/StudentAccountContract.java
@@ -1,4 +1,4 @@
-package com.example.mosadi.chefschool.mysqllite;
+package com.example.mosadi.chefschool.userinformation;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
@@ -9,9 +9,9 @@ import android.provider.BaseColumns;
  * Created by Mosadi on 2017/09/04.
  */
 
-public class FeedReaderContract r{
+public class StudentAccountContract {
     //make the constructor private
- 
+
         //public static final String COLUMN_NAME_= "";
 
     /* Inner class that defines the table contents */
@@ -22,6 +22,7 @@ public class FeedReaderContract r{
         public static final String COLUMN_NAME_TIME = "date";
         public static final String COLUMN_NAME_VENUE = "venue";
         public static final String COLUMN_NAME_RSVP = "RSVP";
+    }
         private static final String SQL_CREATE_EVENTS_ENTRIES =
                 "CREATE TABLE " + EventsEntry.TABLE_NAME + " (" +
                         EventsEntry._ID + " INTEGER PRIMARY KEY," +
@@ -33,13 +34,14 @@ public class FeedReaderContract r{
 
         private static final String SQL_DELETE_EVENTSENTRY =
                 "DROP TABLE IF EXISTS " + EventsEntry.TABLE_NAME;
-    }
+
     //a table to store all the user's loginDetails
-    public static class LoginDetailsEntry implements BaseColumns{
+    public static class LoginDetailsEntry implements BaseColumns {
         public static final String TABLE_NAME = "loginDetails";
-        public static final String COLUMN_NAME_ID= "User_ID";
-        public static final String COLUMN_NAME_USERNAME= "Username";
-        public static final String COLUMN_NAME_PASSWORD= "Password";
+        public static final String COLUMN_NAME_ID = "User_ID";
+        public static final String COLUMN_NAME_USERNAME = "Username";
+        public static final String COLUMN_NAME_PASSWORD = "Password";
+    }
         private static final String SQL_CREATE_lOGIN_ENTRIES =
                 "CREATE TABLE " + LoginDetailsEntry.TABLE_NAME + " (" +
                         EventsEntry._ID + " INTEGER PRIMARY KEY," +
@@ -49,7 +51,7 @@ public class FeedReaderContract r{
 
         private static final String SQL_DELETE_LOGINDETAILS =
                 "DROP TABLE IF EXISTS " + LoginDetailsEntry.TABLE_NAME;
-    }
+
 
 
     //A table to store all the profile entries
@@ -57,17 +59,19 @@ public class FeedReaderContract r{
         public static final String TABLE_NAME = "profiles";
         public static final String COLUMN_NAME_NAME = "name";
         public static final String COLUMN_NAME_SURNAME = "surname";
+        public static final String COLUMN_NAME_PICTURE = "picture";
         public static final String COLUMN_NAME_EMAIL = "email";
         public static final String COLUMN_NAME_PHONE = "Phone";
         public static final String COLUMN_NAME_CLASS = "class";
         public static final String COLUMN_NAME_WORK_STATUS = "work_status";
-        public static final String COLUMN_NAME_DOB= "date-of-birth";
-        //CREATE THE TABLE
+        public static final String COLUMN_NAME_DOB = "date-of-birth";
+    }
         private static final String SQL_CREATE_PROFILE_ENTRIES =
            "CREATE TABLE " + ProfileEntry.TABLE_NAME + " (" +
                    ProfileEntry._ID + " INTEGER PRIMARY KEY," +
                    ProfileEntry.COLUMN_NAME_NAME + " TEXT," +
                    ProfileEntry.COLUMN_NAME_SURNAME + " TEXT," +
+                   ProfileEntry.COLUMN_NAME_PICTURE + " TEXT," +
                    ProfileEntry.COLUMN_NAME_EMAIL + " TEXT," +
                    ProfileEntry.COLUMN_NAME_PHONE + " TEXT," +
                    ProfileEntry.COLUMN_NAME_CLASS + " TEXT," +
@@ -77,14 +81,15 @@ public class FeedReaderContract r{
                 "DROP TABLE IF EXISTS " + ProfileEntry.TABLE_NAME;
 
 
-    }
+
     //a table to store all the user addresses
-    public static class AddressEntry implements BaseColumns{
+    public static class AddressEntry implements BaseColumns {
         public static final String TABLE_NAME = "address";
         public static final String COLUMN_NAME_COUNTRY = "country";
-        public static final String COLUMN_NAME_CITY= "City";
-        public static final String COLUMN_NAME_PROVINCE= "Province";
-        public static final String COLUMN_NAME_SURBURB= "Surburb";
+        public static final String COLUMN_NAME_CITY = "City";
+        public static final String COLUMN_NAME_PROVINCE = "Province";
+        public static final String COLUMN_NAME_SURBURB = "Surburb";
+    }
         private static final String SQL_CREATE_ADDRESS_ENTRIES =
                 "CREATE TABLE " + AddressEntry.TABLE_NAME + " (" +
                         AddressEntry._ID + " INTEGER PRIMARY KEY," +
@@ -94,6 +99,43 @@ public class FeedReaderContract r{
                         AddressEntry.COLUMN_NAME_SURBURB+ " TEXT)" ;
         private static final String SQL_DELETE_ADDRESS =
                 "DROP TABLE IF EXISTS " + AddressEntry.TABLE_NAME;
+
+    //the sdqlite helper class
+    public class StudentsAccountDbHelper extends SQLiteOpenHelper {
+        public static final int DATABASE_VERSION = 1;
+        public static final String DATABASE_NAME = "ictstudents.db";
+
+        private StudentsAccountDbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_CREATE_EVENTS_ENTRIES);
+            db.execSQL(SQL_CREATE_lOGIN_ENTRIES);
+            db.execSQL(SQL_CREATE_PROFILE_ENTRIES);
+            db.execSQL(SQL_CREATE_ADDRESS_ENTRIES);
+        }
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // This database is only a cache for online data, so its upgrade policy is
+            // to simply to discard the data and start over
+            db.execSQL(SQL_DELETE_EVENTSENTRY);
+            db.execSQL(SQL_DELETE_LOGINDETAILS);
+            db.execSQL(SQL_DELETE_PROFILE);
+            db.execSQL(SQL_DELETE_ADDRESS);
+            onCreate(db);
+        }
+        @Override
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            onUpgrade(db, oldVersion, newVersion);
+        }
+
     }
+    //ACCESS THE DB
+   // StudentsAccountDbHelper mDbHelper = new StudentsAccountDbHelper(getContext());
+// Gets the data repository in write mode
+   // SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+    // Create a new map of values, where column names are the keys
 
 }
